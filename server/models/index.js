375dafbd46c82b797d2a7ee534134cdff783c6a8
eaeidsath <evelyneidsath@gmail.com>
2024-04-10 01:07:49 -0700

    adjust model index.js

--- server/models/index.js
+++ server/models/index.js
@@ -2,6 +2,5 @@ const User = require('./User');
 const Goal = require('./Goal');
 const Activity = require('./Activity');
 const Post = require('./Post');
-const Comment = require('./Comment');
 
-module.exports = { User, Goal, Activity, Post, Comment };
+module.exports = { User, Goal, Activity, Post };
