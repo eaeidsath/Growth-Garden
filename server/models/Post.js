da5520295c26f4234992ee0375b60996df494869
eaeidsath <evelyneidsath@gmail.com>
2024-04-10 01:03:06 -0700

    finish post model, remove comment model and include in post model

---
+++ server/models/Post.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema({
+  postText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    maxlength: 280,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    get: (date) => date.toLocaleString(),
+  },
+  comments: [
+    {
+      commentText: {
+        type: String,
+        required: true,
+        minlength: 1,
+        maxlength: 280,
+      },
+      commentAuthor: {
+        type: String,
+        required: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (date) => date.toLocaleString(),
+      },
+    },
+  ],
+});
+
+const Post = model('post', postSchema);
+
+module.exports = Post;
