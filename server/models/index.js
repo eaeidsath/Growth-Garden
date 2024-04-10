e0dcdc1aca3ed87a271ba710357969c72cc19203
eaeidsath <evelyneidsath@gmail.com>
2024-04-09 20:50:31 -0700

    fill out models index.js, add model files for user, goal, activity, post, comment

---
+++ server/models/index.js
@@ -0,0 +1,7 @@
+const User = require('./User');
+const Goal = require('./Goal');
+const Activity = require('./Activity');
+const Post = require('./Post');
+const Comment = require('./Comment');
+
+module.exports = { User, Goal, Activity, Post, Comment };
