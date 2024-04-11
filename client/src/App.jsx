043691ba93135802cec23eab38cb4bf22fb32c45
Benita <benita.isaac@gmail.com>
2024-04-11 00:35:55 -0700

    added Outlet for navbar functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-import "./index.css"
+import Navbar from "./components/Navbar";
+import "./index.css";
+import { Outlet, useLocation } from "react-router-dom";
 
 export default function App() {
+  const currentPage=useLocation().pathname;
+
   return (
     <>
       <h1 className="rubik-scribble-regular">Growth Garden</h1>
+      <Navbar />
+      <Outlet />
     </>
   )
 }
