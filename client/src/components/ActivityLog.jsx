9d7640f0c1aaa6b3bcba6f30d5ac38375b525526
Benita <benita.isaac@gmail.com>
2024-04-11 18:52:25 -0700

    created activity log component and added code

---
+++ client/src/components/ActivityLog.jsx
@@ -0,0 +1,25 @@
+export function ActivityLog() {
+  return (
+    <>
+      <div className="activityLogMain">
+        {/* TODO: Add dynamic JS variable here (refer to models) */}
+        <h2> Goal:  </h2>
+        <div>
+            {/* TODO: add flex properties so these are displayed 'space between' */}
+            <h3>Activity Log</h3>
+            <button> +New Activity </button>
+            <div>
+                {/* TODO: add dynamic JS variable for progress towards goals (look at models) */}
+                <ul>
+                    <li> Progress towards goal 1 </li>
+                    <li> Progress towards goal 1 </li>
+                    <li> Progress towards goal 1 </li>
+                </ul>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ActivityLog;
