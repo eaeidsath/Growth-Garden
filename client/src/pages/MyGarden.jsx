d94aa7023f8974026a292fb75817939f6d3e9fd0
EE <evelyneidsath@gmail.com>
2024-04-15 20:44:15 -0700

    add function to render flower component, update mygarden, move flower component into its own folder

--- client/src/pages/MyGarden.jsx
+++ client/src/pages/MyGarden.jsx
@@ -1,40 +1,43 @@
 //Components needed in this file:
 // Flower.jsx (for an individual flower)
 // MyGoals.jsx (to display the user's goal log)
-// ActivityLog.jsx (for a user to document progress towards a goal)
 // CreateNewGoal.jsx (form which allows a user to create a new goal)
 
 //This file will hold the logic for rendering the growth of the garden (so this will have some html)
 // We should be able to map through an array to display all of the flowers in the general garden. We can use the code from the portfolio to help us with this.
 
-// onClick logic will be in this page to be able to see an individual flower
-import Flower from "../components/Flower";
+// link to view individual goal page is in the Flower.jsx component
+import Flower from "../components/Flower/Flower";
 
 export default function MyGarden() {
+  //this is representing the 'goals' array from a user's goals
   const flowers = [
     {
       id: 1,
       name: "daisy",
       goal: "Complete five push-ups",
-      img: "1",
+      activityLevel: 0
+  
     },
     {
       id: 2,
       name: "rose",
       goal: "Run the 5K",
-      img: "2",
+      activityLevel: 4
+ 
     },
     {
       id: 3,
       name: "daisy",
       goal: "Read a book",
-      img: "1",
+      activityLevel: 6
+
     },
     {
       id: 4,
       name: "petunia",
       goal: "Finish portfolio",
-      img: "3",
+      activityLevel: 6
     },
   ];
 
@@ -46,7 +49,7 @@ export default function MyGarden() {
           id={flower.id}
           name={flower.name}
           goal={flower.goal}
-          img={flower.img}
+          activityLevel={flower.activityLevel}
         />
       ))}
     </div>
