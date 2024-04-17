80c5bf61f6ced17b979f73f3a575edac27e9acc2
Benita <benita.isaac@gmail.com>
2024-04-17 10:07:41 -0700

    working on functionality of create new goal form

--- client/src/components/CreateNewGoal/CreateNewGoal.jsx
+++ client/src/components/CreateNewGoal/CreateNewGoal.jsx
@@ -1,97 +1,147 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-export default function CreateNewGoal() {
+import { useMutation } from "@apollo/client";
+import jwt_decode from 'jwt-decode';
+import { ADD_GOAL } from "../../utils/mutations";
+
+import Auth from "../../utils/auth";
+
+import { Link } from 'react-router-dom';
+
+
+export default function CreateNewGoal({ username }) {
   const [formData, setFormData] = useState({
-    categoryInput: "",
-    goalTitleInput: "",
-    frequencyInput: "",
+    category: "",
+    goalTitle: "",
+    frequency: "",
   });
 
   const handleDropdownChange = (event) => {
-    setFormData({ ...formData, categoryInput: event.target.value });
+    setFormData({ ...formData, category: event.target.value });
   };
 
   const handleTextInputChange = (event) => {
-    setFormData({ ...formData, goalTitleInput: event.target.value });
+    setFormData({ ...formData, goalTitle: event.target.value });
   };
 
   const handleRadioChange = (event) => {
-    setFormData({ ...formData, frequencyInput: event.target.value });
+    setFormData({ ...formData, frequency: event.target.value });
   };
 
-  const handleSubmit = (event) => {
+  const [addGoal, { error }] = useMutation(ADD_GOAL);
+
+  useEffect(() => {
+    // Get the JWT token from local storage
+    const token = localStorage.getItem('jwtToken');
+    if (token) {
+      // Decode the token to extract the username
+      const decodedToken = jwt_decode(token);
+      const username = decodedToken.username;
+      // Use the username if needed
+      console.log('Username:', username);
+    }
+  }, []);
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
     console.log(formData);
+
+    try {
+      const data = await addGoal({
+        variables: { username: getUsernameFromToken(),  category: formData.category, goalTitle: formData.goalTitle, frequency: formData.frequency },
+      });
+      setFormData("");
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const getUsernameFromToken = () => {
+    const token = localStorage.getItem('jwtToken');
+    if (token) {
+      const decodedToken = jwt_decode(token);
+      return decodedToken.username;
+    }
+    return null;
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        What area would you like to improve?
-        <br />
-        <select value={formData.categoryInput} onChange={handleDropdownChange}>
-          <option value="">-- Choose a category --</option>
-          <option value="fitness">Fitness </option>
-          <option value="finance">Finance</option>
-          <option value="mindfulness">Mindfulness</option>
-          <option value="career">Career</option>
-          <option value="education">Education</option>
-          <option value="skills">Skill-building</option>
-          <option value="travel">Travel</option>
-          <option value="goodwill">Goodwill</option>
-          <option value="productivity">Productivity</option>
-        </select>
-      </label>
-      <br />
-      <br />
-
-      <label>
-        What is your personal goal in this area?
-        <br />
-        <input
-          type="text"
-          value={formData.goalTitleInput}
-          onChange={handleTextInputChange}
-        />
-      </label>
-      <br />
-      <br />
-
-      <label>
-        How often can you contribute activities towards this goal?
-        <br />
-        <br />
+    <>
+    {Auth.loggedIn() ? (
+      <form onSubmit={handleSubmit}>
         <label>
-          <input
-            type="radio"
-            value="daily"
-            checked={formData.frequencyInput === "daily"}
-            onChange={handleRadioChange}
-          />
-          daily
+          What area would you like to improve?
+          <br />
+          <select value={formData.categoryInput} onChange={handleDropdownChange}>
+            <option value="">-- Choose a category --</option>
+            <option value="fitness">Fitness </option>
+            <option value="finance">Finance</option>
+            <option value="mindfulness">Mindfulness</option>
+            <option value="career">Career</option>
+            <option value="education">Education</option>
+            <option value="skills">Skill-building</option>
+            <option value="travel">Travel</option>
+            <option value="goodwill">Goodwill</option>
+            <option value="productivity">Productivity</option>
+          </select>
         </label>
-        <label>
-          <input
-            type="radio"
-            value="weekly"
-            checked={formData.frequencyInput === "weekly"}
-            onChange={handleRadioChange}
-          />
-          weekly
-        </label>
-        <label>
-          <input
-            type="radio"
-            value="monthly"
-            checked={formData.frequencyInput === "monthly"}
-            onChange={handleRadioChange}
-          />
-          monthly
-        </label>
-      </label>
-      <br />
+        <br />
+        <br />
 
-      <button type="submit">Submit</button>
-    </form>
+        <label>
+          What is your personal goal in this area?
+          <br />
+          <input
+            type="text"
+            value={formData.goalTitleInput}
+            onChange={handleTextInputChange}
+          />
+        </label>
+        <br />
+        <br />
+
+        <label>
+          How often can you contribute activities towards this goal?
+          <br />
+          <br />
+          <label>
+            <input
+              type="radio"
+              value="daily"
+              checked={formData.frequency === "daily"}
+              onChange={handleRadioChange}
+            />
+            daily
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="weekly"
+              checked={formData.frequency === "weekly"}
+              onChange={handleRadioChange}
+            />
+            weekly
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="monthly"
+              checked={formData.frequency === "monthly"}
+              onChange={handleRadioChange}
+            />
+            monthly
+          </label>
+        </label>
+        <br />
+
+        <button type="submit">Submit</button>
+      </form>
+      ) : (
+        <p> You need to be logged in to make a goal. Please{' '}
+        <Link to="/">login</Link> or  
+        <Link to="/signup"> signup.</Link>
+         </p>
+      )}
+    </>
   );
-};
+}
