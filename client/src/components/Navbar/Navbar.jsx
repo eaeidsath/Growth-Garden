767df172543b96485da2faa5de726e746cac5ec3
Benita <benita.isaac@gmail.com>
2024-04-17 13:16:15 -0700

    added login/logout button logic

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,14 @@
 import { Link, useLocation } from "react-router-dom";
 import { Nav, NavButton, NavLinks, NavLink } from "./Navbar.styles";
 
+import Auth from "../../utils/auth";
+
 function Navbar() {
+  //create logout function
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
   const currentPage = useLocation().pathname;
 
   // Function to handle click event on navigation items
@@ -15,9 +22,7 @@ function Navbar() {
         <li>
           <NavLink
             to="/dashboard"
-            className={
-              currentPage === "/dashboard" ? "active" : ""
-            }
+            className={currentPage === "/dashboard" ? "active" : ""}
             onClick={() => handleNavigationClick("dashboard")}
           >
             Dashboard
@@ -26,9 +31,7 @@ function Navbar() {
         <li>
           <NavLink
             to="/mygarden"
-            className={
-              currentPage === "/mygarden" ? "active" : ""
-            }
+            className={currentPage === "/mygarden" ? "active" : ""}
             onClick={() => handleNavigationClick("mygarden")}
           >
             My Garden
@@ -37,18 +40,28 @@ function Navbar() {
         <li>
           <NavLink
             to="/explore"
-            className={
-              currentPage === "/explore" ? "active" : ""
-            }
+            className={currentPage === "/explore" ? "active" : ""}
             onClick={() => handleNavigationClick("explore")}
           >
             Explore
           </NavLink>
         </li>
-        <li>
-          {/* <Link to="/logout">Log Out</Link> */}
-          <NavButton> Log Out </NavButton>
-        </li>
+        <div>
+          {Auth.loggedIn() ? (
+            <li>
+              <NavButton onClick={logout}> Log Out </NavButton>
+            </li>
+          ) : (
+            <>
+              <li>
+                <NavButton>
+                  {" "}
+                  <Link to="/">Log In</Link>{" "}
+                </NavButton>
+              </li>
+            </>
+          )}
+        </div>
       </NavLinks>
     </Nav>
   );
