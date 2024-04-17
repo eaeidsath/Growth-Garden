e36224e44131ce741e3b418af674de5c5c4257c7
Keri Sen <147566619+kerilsen@users.noreply.github.com>
2024-04-16 18:46:43 -0700

    Added comet chat to the dependencies

---
+++ client/src/pages/CometChat/App.js
@@ -0,0 +1,5 @@
+import { CometChatUsersWithMessages } from "@cometchat/chat-uikit-react"; //import the component in your App.js file
+
+export const App = () => {
+  return <CometChatUsersWithMessages />;
+}; 
