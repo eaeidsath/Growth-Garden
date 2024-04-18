1a1a9c4e038841b3e61a6f0b8713999919b66c57
eaeidsath <evelyneidsath@gmail.com>
2024-04-18 03:40:17 -0700

    add more flower stage images, adjust flower component function

--- client/src/components/Flower/Flower.jsx
+++ client/src/components/Flower/Flower.jsx
@@ -13,18 +13,22 @@ export default function Flower({ _id, flowerType, goalTitle, activityLevel }) {
 
   const flowerGrowth = (x) => {
     if (x === 0) {
-      setFlowerStage('flower_pot.png');
-      console.log(x);
-    } else if ((0 < x) && (x <= 4)) {
       setFlowerStage('stage_1.png');
-      console.log(x);
-    } else if ((4 < x) && (x <= 8)) {
+    } else if ((0 < x) && (x <= 3)) {
       setFlowerStage('stage_2.png');
-      console.log(x);
+    } else if ((3 < x) && (x <= 5)) {
+      setFlowerStage('stage_3.png');
+    } else if ((5 < x) && (x <= 8)) {
+      setFlowerStage('stage_4.png');
+    } else if ((8 < x) && (x <= 12)) {
+      setFlowerStage('stage_5.png');
+    } else if ((12 < x) && (x <= 15)) {
+      setFlowerStage('stage_6.png');
+    } else if ((15 < x)) {
+      setFlowerStage('bloom.png');
     }
   }
 
-  // change link address to `/goals/${_id}` when main is updated
   return (
     <FlowerCard>
         <Link to={`/singlegoal/${_id}`}>
