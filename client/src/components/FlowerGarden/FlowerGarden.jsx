457aaa03e75b5744c8408cf2f65601de253491b8
Keri Sen <147566619+kerilsen@users.noreply.github.com>
2024-04-17 23:08:25 -0700

    Fixed styling on MyGarden

--- client/src/components/FlowerGarden/FlowerGarden.jsx
+++ client/src/components/FlowerGarden/FlowerGarden.jsx
@@ -1,27 +1,24 @@
-import Flower from '../Flower/Flower';
+import Flower from "../Flower/Flower";
 
-const FlowerGarden = ({
-    goals
-}) => {
-    if (!goals.length) {
-      return <h3>No goals have been created.</h3>
-    }
+const FlowerGarden = ({ goals }) => {
+  if (!goals.length) {
+    return <h3>No goals have been created.</h3>;
+  }
 
-
-return (
-    <div className="grid-container garden">
-      {goals && goals.map((goal) => (
-        <div key={goal._id}>
-        <Flower
-          _id={goal._id}
-          flowerType={goal.flowerType}
-          goalTitle={goal.goalTitle}
-          activityLevel={goal.activityLevel}
-        />
-        </div>
-      ))}
-    </div>
+  return (
+    <>
+      {goals &&
+        goals.map((goal) => (
+          <Flower
+            key={goal._id}
+            _id={goal._id}
+            flowerType={goal.flowerType}
+            goalTitle={goal.goalTitle}
+            activityLevel={goal.activityLevel}
+          />
+        ))}
+    </>
   );
-}
+};
 
 export default FlowerGarden;
--- client/src/pages/MyGarden/MyGarden.jsx
+++ client/src/pages/MyGarden/MyGarden.jsx
@@ -1,22 +1,19 @@
-import { useQuery } from '@apollo/client';
+import { useQuery } from "@apollo/client";
 
 import MyGoals from "../../components/MyGoals/MyGoals";
-import FlowerGarden from '../../components/FlowerGarden/FlowerGarden';
+import FlowerGarden from "../../components/FlowerGarden/FlowerGarden";
 
-import { LargeContainer, GardenBox, GoalCard } from './MyGarden.styles';
+import { LargeContainer, GardenBox, GoalCard } from "./MyGarden.styles";
 
-import { QUERY_ME } from '../../utils/queries';
-
+import { QUERY_ME } from "../../utils/queries";
 
 export default function MyGarden() {
-
   const { loading, data } = useQuery(QUERY_ME);
 
   const user = data?.me || {};
 
-
-  if(loading) {
-    return <div>Loading...</div>
+  if (loading) {
+    return <div>Loading...</div>;
   }
 
   if (!user?.username) {
@@ -29,18 +26,21 @@ export default function MyGarden() {
   }
 
   return (
-    <LargeContainer>
+    <>
       <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Welcome, {user.username}.
+        Welcome, {user.username}.
       </h2>
-      <GardenBox>
-          <FlowerGarden
-            goals={user.goals}
-          />
-      </GardenBox>
-      <GoalCard>
-        <MyGoals />
-      </GoalCard>
-    </LargeContainer>
+      <LargeContainer>
+        
+        <GardenBox>
+          <FlowerGarden goals={user.goals} />
+        </GardenBox>
+
+        <GoalCard>
+          <MyGoals />
+        </GoalCard>
+
+      </LargeContainer>
+    </>
   );
 }
