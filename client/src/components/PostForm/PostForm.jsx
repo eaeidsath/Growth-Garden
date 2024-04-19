ad2f86064020caf300c5cf5307724ba71a2101f8
eaeidsath <evelyneidsath@gmail.com>
2024-04-19 03:46:10 -0700

    new post form now functional, blurred grass background

--- client/src/components/PostForm/PostForm.jsx
+++ client/src/components/PostForm/PostForm.jsx
@@ -1,56 +1,71 @@
 import { useState } from "react";
+import { Link } from 'react-router-dom';
 import { NoteText, Category, SubmitButton } from './PostForm.styles';
+import { useMutation } from "@apollo/client";
+
+import { ADD_POST } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
 
 export default function PostForm() {
-  const [formData, setFormData] = useState({
-    // username
-    postTextInput: "",
-    categoryValue: "",
-  });
+  const [postText, setPostText] = useState('');
 
-  const handleTextInputChange = (event) => {
-    setFormData({ ...formData, postTextInput: event.target.value });
+  const [addPost, { error }] = useMutation(ADD_POST)
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'postText' && value.length <= 280) {
+      setPostText(value);
+    }
   };
 
-  const handleDropdownChange = (event) => {
-    setFormData({ ...formData, categoryValue: event.target.value });
-  };
-
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(formData);
+    try {
+      const { data } = await addPost({
+        variables: {
+          postText,
+          username: Auth.getProfile().data.username
+        },
+      });
+
+      setPostText('');
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        How's it going?
-        <NoteText
-          value={formData.postTextInput}
-          onChange={handleTextInputChange}
-          maxLength={280}
-          rows={6}
-          cols={50}
-        />
-      </label>
-      <br />
-      <label>
-        What goal are you working on?
-        <Category value={formData.categoryValue} onChange={handleDropdownChange}>
-          <option value="">-- Choose a category --</option>
-          <option value="fitness">Fitness </option>
-          <option value="finance">Finance</option>
-          <option value="mindfulness">Mindfulness</option>
-          <option value="career">Career</option>
-          <option value="education">Education</option>
-          <option value="skills">Skill-building</option>
-          <option value="travel">Travel</option>
-          <option value="goodwill">Goodwill</option>
-          <option value="productivity">Productivity</option>
-        </Category>
-      </label>
-      <br />
-      <SubmitButton type="submit">Post</SubmitButton>
-    </form>
+    <div>
+      {Auth.loggedIn() ? (
+        <div>
+          <form onSubmit={handleSubmit}>
+            <label>
+              How's it going?
+            </label>
+            <NoteText
+              value={postText}
+              onChange={handleChange}
+              name='postText'
+              maxLength={280}
+              rows={6}
+              cols={50}
+            />
+
+            <br />
+
+            <br />
+            <SubmitButton type="submit">Post</SubmitButton>
+          </form>
+        </div>
+      ) : (
+        <p>
+          You need to be logged in to share posts. Please{' '}
+          <Link to="/">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )
+      }
+    </div >
   );
 }
