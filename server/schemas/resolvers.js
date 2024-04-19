0b83f8b52620bf8f902df28e3391855e58f22d7f
Keri Sen <147566619+kerilsen@users.noreply.github.com>
2024-04-19 10:34:54 -0700

    Fixing a typo

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -206,7 +206,7 @@ const resolvers = {
             }
             throw AuthenticationError;
         }
-    updateGoal: async (parent, { goalId, updatedGoal }, context) => {
+    // updateGoal: async (parent, { goalId, updatedGoal }, context) => {
     //   if (context.user) {
     //     // Ensure that the user owns the goal
     //     const goal = await Goal.findOne({
@@ -219,14 +219,14 @@ const resolvers = {
     //     }
 
         // Update the goal with the new data
-        const updated = await Goal.findOneAndUpdate(
-          { _id: goalId },
-          { goalTitle: updatedGoal },
-          { new: true }
-        );
+    //     const updated = await Goal.findOneAndUpdate(
+    //       { _id: goalId },
+    //       { goalTitle: updatedGoal },
+    //       { new: true }
+    //     );
 
-        return updated;
-      }
+    //     return updated;
+    //   }
     //   throw AuthenticationError;
     // },
   },
