8cca3f3eb7803dac9db086572046f053be907184
Benita <benita.isaac@gmail.com>
2024-04-20 14:22:18 -0700

    reverting back to old code

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -6,8 +6,9 @@ export default function Header() {
     <>
       <TopBanner>
         {/* <Title>Growth Garden</Title> */}
-        <Navbar />
+        
       </TopBanner>
+      <Navbar />
     </>
   );
 }
