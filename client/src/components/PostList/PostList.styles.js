52997e5a94ec4873a9013d1e16f98c701584d9b6
eaeidsath <evelyneidsath@gmail.com>
2024-04-20 14:37:03 -0700

    fix post stacking on dashboard

--- client/src/components/PostList/PostList.styles.js
+++ client/src/components/PostList/PostList.styles.js
@@ -1,5 +1,11 @@
 import { styled } from "styled-components";
 
+export const MessageGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+  gap: 10px;
+`;
+
 export const MessageItem = styled.div`
   padding: 10px;
   text-align: center;
--- client/src/pages/Dashboard/Dashboard.styles.js
+++ client/src/pages/Dashboard/Dashboard.styles.js
@@ -33,15 +33,3 @@ export const MessageBoard = styled.div`
     font-family: var(--quicksand)
   }
 `;
-
-export const MessageGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-  gap: 10px;
-`;
-
-export const MessageItem = styled.div`
-  padding: 10px;
-  text-align: center;
-  vertical-align: bottom;
-`;
