52416244a7aa75419df1e385a24ab02c053ad3fc
Benita <benita.isaac@gmail.com>
2024-04-20 22:46:37 -0700

    added messages component

---
+++ client/src/components/Messaging/Messaging.styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #222222;
+  color: white;
+  display: inline-flex;
+  padding-left: 120px;
+  padding-right: 120px;
+  justify-content: flex-end;
+  align-items: flex-end;
+  margin-top: 10px;
+  margin-left: 2px;
+`;
