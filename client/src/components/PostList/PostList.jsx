4a5d11638f772bb4d667d0f43f3b3760d4abe93c
eaeidsath <evelyneidsath@gmail.com>
2024-04-21 04:09:30 -0700

    fix typo

--- client/src/components/PostList/PostList.jsx
+++ client/src/components/PostList/PostList.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { MessageItem, MessageGrid, Chalk } from "./PostList.styles"
+import { MessageItem, MessageGrid, Chalk } from "./PostList.styles";
 
 const PostList = ({
     friends
