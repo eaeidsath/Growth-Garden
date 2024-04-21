735ce76fe540f691e91cc1867952ca8801870779
eaeidsath <evelyneidsath@gmail.com>
2024-04-21 04:55:15 -0700

    adjust position of messenger widget

--- client/src/components/Messaging/Messaging.styles.js
+++ client/src/components/Messaging/Messaging.styles.js
@@ -1,8 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  background-color: #222222;
+  background-color: var(--dark-moss-green);
   color: white;
+  cursor: pointer;
   display: inline-flex;
   padding-left: 120px;
   padding-right: 120px;
@@ -10,4 +11,17 @@ export const Container = styled.div`
   align-items: flex-end;
   margin-top: 10px;
   margin-left: 2px;
+  margin-bottom: 70px; 
+  border: 2px solid white;
+  border-radius: 5px;
+  bottom: 0;
+  right: 5px;
+  position: fixed;
+  @media (max-width: 1000px) {
+    width: 20px;
+    padding-left: 25px;
+    padding-right: 25px;
+    color: transparent;
+    overflow: hidden;
+  }
 `;
