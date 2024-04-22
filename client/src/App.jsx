bca0fedc8620c562ae10a6d3bc73d20851f262a7
eaeidsath <evelyneidsath@gmail.com>
2024-04-21 17:08:57 -0700

    removing messaging widget

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import GlobalStyle from "./assets/Global.style";
-import Messaging from "./components/Messaging/Messaging";
+
 import styled from "styled-components";
 import { Outlet, useLocation } from "react-router-dom";
 
@@ -48,7 +48,7 @@ export default function App() {
         <Header />
         <Main>
           <Outlet />
-          <Messaging />
+
         </Main>
         <Footer />
       </ApolloProvider>
